29d93a38f3b26d228a9bcc57b13cda2a21a02691
CR <robert.bilkevic@gmail.com>
2024-07-28 21:10:13 +0300

    Add Homework4 stubs

---
+++ Source/Chapter1/Homework4/Checks.cs
@@ -0,0 +1,37 @@
+﻿namespace Homework4
+{
+    public static class Checks
+    {
+        public static int PromptInt(string message)
+        {
+            Console.WriteLine(message);
+            var stringInput = Console.ReadLine();
+
+            if (!int.TryParse(stringInput, out var intInput)) throw new ArgumentException("Invalid input. Please enter a number.");
+            return intInput;
+        }
+
+        public static string PromptString(string message)
+        {
+            Console.WriteLine(message);
+            var stringInput = Console.ReadLine() ?? throw new ArgumentException("Invalid input. Please enter a string.");
+            
+            return stringInput;
+        }
+
+        public static float PromptFloat(string message)
+        {
+            Console.WriteLine(message);
+            var stringInput = Console.ReadLine();
+
+            if (!float.TryParse(stringInput, out var floatInput)) throw new ArgumentException("Invalid input. Please enter a number.");
+            return floatInput;
+        }
+
+        public static float CalculateBmi(float weight, float height)
+        {
+            if (height == 0) throw new ArgumentException("Height cannot be 0.");
+            return weight / (height * height);
+        }
+    }
+}
---
+++ Tests/Chapter1.Tests/Utils/TestInputSanitizer.cs
@@ -0,0 +1,7 @@
+﻿namespace Chapter1.Tests.Utils
+{
+    public static class TestInputSanitizer
+    {
+        public static string[] ToNewlineSentences(this string message) => message.Split(".");
+    }
+}
