356ea192a0c75bedd50acff721aed69534cfbd2e
CR <robert.bilkevic@gmail.com>
2024-07-29 19:17:11 +0300

    Add TextTable solution

--- Source/Chapter1/Homework7/TextTable.cs
+++ Source/Chapter1/Homework7/TextTable.cs
@@ -1,39 +1,63 @@
-﻿namespace Homework7;
+﻿using System.Text;
 
-/// <summary>
-/// Part 1.
-/// </summary>
+namespace Homework7;
+
 public static class TextTable
 {
-    /*
-
-     Input: "Hello", 0
-       +-----+
-       |Hello|
-       +-----+
-
-     Input: $"Hello{Environment.NewLine}World!", 0
-       +------+
-       |Hello |
-       |World!|
-       +------+
-
-     Input: "Hello", 1
-       +-------+
-       |       |
-       | Hello |
-       |       |
-       +-------+
-
-     */
-
-    /// <summary>
-    /// Build a table for given message with given padding.
-    /// Padding means how many spaces will a message be wrapped with.
-    /// Table itself is made of: "+-" symbols. 
-    /// </summary>
     public static string Build(string message, int padding)
     {
-        return "";
+        if (string.IsNullOrEmpty(message))
+        {
+            return string.Empty;
+        }
+
+        var words = message.Split(Environment.NewLine);
+        var longestWordLength = GetLongestWordLength(words);
+        var length = longestWordLength + padding * 2;
+
+        var table = new StringBuilder();
+        table.AppendLine(BuildBorder(length));
+        table.Append(BuildPaddingRows(length, padding));
+        table.Append(BuildContentRows(words, longestWordLength, padding));
+        table.Append(BuildPaddingRows(length, padding));
+        table.AppendLine(BuildBorder(length));
+
+        return table.ToString();
+    }
+
+    private static string BuildBorder(int length)
+    {
+        return "+" + new string('-', length) + "+";
+    }
+
+    private static string BuildPaddingRows(int length, int padding)
+    {
+        var paddingRow = "|" + new string(' ', length) + "|";
+        var paddingRows = new StringBuilder();
+        for (var i = 0; i < padding; i++)
+        {
+            paddingRows.AppendLine(paddingRow);
+        }
+
+        return paddingRows.ToString();
+    }
+
+    private static string BuildContentRows(string[] words, int longestWordLength, int padding)
+    {
+        var contentRows = new StringBuilder();
+        foreach (var word in words)
+        {
+            contentRows.AppendLine("|"
+                                   + new string(' ', padding)
+                                   + word.PadRight(longestWordLength)
+                                   + new string(' ', padding) + "|");
+        }
+
+        return contentRows.ToString();
+    }
+
+    private static int GetLongestWordLength(string[] words)
+    {
+        return words.Max(word => word.Length);
     }
 }
